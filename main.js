c3c190d9be58a4e09716560bdfe18f3940500e8d
sejinniemma <jsj0471@naver.com>
2022-07-13 07:10:18 +0900

    add Math round to scroll position

--- main.js
+++ main.js
@@ -148,13 +148,15 @@ const observer = new IntersectionObserver(observerCallback, observerOptions)
 sections.forEach((section) => observer.observe(section))
 
 window.addEventListener('wheel', () => {
+  console.log( window.scrollY + window.innerHeight , document.body.clientHeight)
+    console.log(window.scrollY,window.innerHeight )
   if (window.scrollY === 0) {
     selectedNavIndex = 0
   } else if (
-    window.scrollY + window.innerHeight ===
-    document.body.clientHeight
+   Math.round( window.scrollY + window.innerHeight) >= document.body.clientHeight
   ) {
     selectedNavIndex = navItems.length - 1
   }
   selectNavItem(navItems[selectedNavIndex])
 })
+
